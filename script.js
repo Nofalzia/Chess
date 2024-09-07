99c6b41865240fa083739798174553ab5e8b33a1
Nofal Zia <nofalzia4S@gmail.com>
2024-09-07 12:50:31 +0500

    Added a new card section

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', function() {
+        this.classList.toggle('flip');
+    });
+});
